38235e59c33e231aef85c4fe8b27e14c873e7a14
mridul.procol <mridul.kumar@procol.in>
2021-06-27 13:01:25 +0530

    Second Commit

--- lib/home_card.dart
+++ lib/home_card.dart
@@ -5,8 +5,8 @@ class HomeCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      backgroundColor: Colors.white,
     );
   }
 }
